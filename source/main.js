d7e5bfee01663e92deae0a25b9ab693bce270393
Epat <kamikadze2185@gmail.com>
2018-07-21 17:19:18 +0200

    get tf2 folder

--- source/main.js
+++ source/main.js
@@ -58,4 +58,23 @@ detectTF2();
 
 let int = setInterval(() => {console.log(count.dis + ' Discord is on: ' + isOn.discord); count.dis += 1; if (isOn.discord) clearInterval(int)}, 500);
 
-let int2 = setInterval(() => {console.log(count.tf2 + ' TF2 is on: ' + isOn.tf2); count.tf2 += 1; if (isOn.tf2) { console.log(JSON.stringify(tf2Exec, null, '\t'));clearInterval(int2)}}, 500);
+let int2 = setInterval(() => {
+  console.log(count.tf2 + ' TF2 is on: ' + isOn.tf2);
+  count.tf2 += 1;
+  if (isOn.tf2) {
+     console.log(JSON.stringify(tf2Exec, null, '\t'));
+     clearInterval(int2);
+     getTf2Folder();
+   }
+ }, 500);
+
+ function getTf2Folder() {
+   let tf2Folder = '';
+   for (i = 0; i < tf2Exec.length; i++) {
+     if (tf2Exec[i].includes('Team Fortress 2')) {
+       let index = tf2Exec[i].lastIndexOf('\\');
+       tf2Folder = tf2Exec[i].slice(0, index + 1);
+     }
+   }
+   console.log('TF2 folder: ' + tf2Folder);
+ }
