57a2c3df205b74eb7e54720e339a65140c3a736a
Epat <kamikadze2185@gmail.com>
2018-07-21 16:20:13 +0200

    detect Discord

--- source/main.js
+++ source/main.js
@@ -1,5 +1,32 @@
 const fs = require('fs');
-const taskMan = require('task-manager');
+const pl = require("process-list");
 const dis = require('./scripts/discord.js');
+const tasks = require('./scripts/tasks.js');
+const ps = require('ps-node');
 
-dis.test();
+const isOn = {
+  discord: false,
+  tf2: false,
+};
+
+function detectDiscord() {
+  ps.lookup({
+    comand: 'discord',
+  },
+  function (err, res) {
+    if (err) {
+      throw new Error(err);
+    }
+    res.forEach( (pr) => {
+      if (pr) {
+        isOn.discord = true;
+      }
+    });
+  }
+);
+}
+
+detectDiscord();
+
+
+let int = setInterval(() => {console.log('Discord is on: ' + isOn.discord); if (isOn.discord) clearInterval(int)}, 500);
