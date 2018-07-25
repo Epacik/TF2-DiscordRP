ea7b7f9e92cbc518fe97e19a9f2cc3e4705728af
Epat <kamikadze2185@gmail.com>
2018-07-25 14:23:33 +0200

    het JSON from url

--- source/main.js
+++ source/main.js
@@ -451,3 +451,18 @@ function detectGamemode(map) {
   }
   return gm;
 }
+
+var getJSON = function(url, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.responseType = 'json';
+    xhr.onload = function() {
+      var status = xhr.status;
+      if (status === 200) {
+        callback(null, xhr.response);
+      } else {
+        callback(status, xhr.response);
+      }
+    };
+    xhr.send();
+};
